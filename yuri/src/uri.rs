d5c368100ac2f022267c921578a2a9471fe47995
pinkforest <36498018+pinkforest@users.noreply.github.com>
2024-11-02 11:49:20 +1300

    Clean-up Unused types

--- yuri/src/uri.rs
+++ yuri/src/uri.rs
@@ -26,23 +26,3 @@ pub struct Uri<'uri> {
     /// Scheme based data
     pub scheme_data: SchemeData<'uri>,
 }
-
-/// By default parsing is strict RFC
-/// Some behaviours can be overriden
-pub enum ParseOverrides {
-    /// Handle User:Password in Userinfo
-    /// RFC 3986 s. 3.2.1. deprecates user:pass
-    /// Default is to Reject
-    /// Application should reject the storage of such data in unencrypted form
-    /// The passing of authentication information in clear text has proven to be
-    /// a security risk in almost every case where it has been used.    
-    DeprecatedPassword(AllowOrIgnore),
-}
-
-/// Allow or ignore type for type storage
-pub enum AllowOrIgnore {
-    /// Allow to store value
-    Allow,
-    /// Ignore and don't store
-    Ignore,
-}
